d80afb487b120bd5c9daa91d52439c4f3f99d5ff
Michael COULLERET <mike20uf@gmail.com>
2016-07-31 11:05:49 +0200

    Add example stage production

---
+++ stages/production.rb
@@ -0,0 +1,51 @@
+##########################################################################
+#
+# This file is part of the demo project.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author Michael COULLERET <michael@coulleret.pro>
+#
+##########################################################################
+
+set :deploy_to, "/project/directory"
+
+set :branch, "master"
+set :symfony_env_prod, "prod"
+
+set :application, "demo"
+set :domain, "ssh.demo.domain"
+set :user, "jenkins"
+
+set :use_sudo, false
+set :keep_releases, 2
+
+role :web, domain
+role :app, domain, :primary => true
+
+# Repository
+set :repository, "ssh://git@gitlab.domain.example"
+set :scm, :git
+set :deploy_via, :copy_with_triggers
+
+## Symfony2
+set :symfony_console, "bin/console"
+set :controllers_to_clear, ["app_*.php"]
+set :shared_files, ["app/config/parameters.yml"]
+set :use_composer, true
+set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader'
+set :composer_dump_autoload_flags, '--optimize'
+set :update_vendors, false
+set :copy_vendors, false
+set :writable_dirs, ["var/cache", "var/logs"]
+set :webserver_user, "www-data"
+set :use_set_permissions, true
+set :dump_assetic_assets, true
+set :model_manager, "doctrine" # ORM
+
+set :build_dir, "#{strategy.destinationrelease}"
+
+on 'strategy:before:compression', 'deploy:assets:bower'
+after 'deploy', 'deploy:assets:bazinga'
+after 'deploy', 'deploy:assets:jsrouting'
